14266bfb39c2e2494c140cc049f946b3f2fe9729
cheri marie <cherimarie@gmail.com>
2024-05-03 18:04:14 -0700

    HTTP request works

---
+++ isbntest.js
@@ -0,0 +1,55 @@
+const fs = require('node:fs');
+
+const https = require('node:https');
+
+const options = {
+  hostname: 'www.googleapis.com',
+  path: '/books/v1/volumes?q=+isbn:9781581574036',
+  method: 'GET',
+};
+const req = https.request(options, (res) => {
+  console.log('statusCode:', res.statusCode);
+
+
+  res.on('data', (response) => {
+    process.stdout.write(response);
+    // let response = JSON.parse(d);
+
+    fs.appendFile('/Users/glaistig/fun-codez/isbnoutput.txt', response, err => {
+            if (err) {
+              console.error("write error", err);
+            } else {
+              // file written successfully
+            }
+          });
+
+    // none of this works. response is not parsable in this way.
+    if(!response.items){console.log("no items :( "); return;}
+
+    for (var i = 0; i < response.items.length; i++) {
+        var item = response.items[i];
+        for (var j = 0; j < item.volumeInfo.industryIdentifiers.length; j++){
+          if(item.volumeInfo.industryIdentifiers[j].identifier == "9781581574036"){
+            console.log("got it")
+            // in production code, item.text should have the HTML entities escaped.
+          let content = `${item.volumeInfo.title} ${item.volumeInfo.subtitle}, ${item.volumeInfo.authors[0]}, ${item.volumeInfo.description}`;
+          }
+
+          fs.appendFile('/Users/glaistig/fun-codez/isbnoutput.txt', content, err => {
+            if (err) {
+              console.error(err);
+            } else {
+              // file written successfully
+            }
+          });
+          console.log("i ", i, " id ", item.volumeInfo.industryIdentifiers[j].identifier)
+        }
+
+      }
+  });
+});
+
+req.on('error', (e) => {
+  console.error(e);
+});
+req.end();
