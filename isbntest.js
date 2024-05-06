eb5facb9f33d6e407b452e51e0ec14e3e31a24a8
cheri marie <cherimarie@gmail.com>
2024-05-05 21:20:57 -0700

    Response is being accumulated properly

--- isbntest.js
+++ isbntest.js
@@ -7,31 +7,22 @@ const options = {
   path: '/books/v1/volumes?q=+isbn:9781581574036',
   method: 'GET',
 };
+let book;
+
 const req = https.request(options, (res) => {
   console.log('statusCode:', res.statusCode);
 
-
   res.on('data', (response) => {
+    book += response;
+  });
 
-    // Qs: why does this execute 3x, though file is only written to 1x? Why does consolelog show buffer but stdout shows real response data?
-    // console.log(response)
-    // console.log("--------console ^ ---- stdout >")
-    // process.stdout.write(response);
-    // let response = JSON.parse(d);
+  res.on('end', () => {
+    console.log(book);
+    // let response = JSON.parse(d); ??
+    if(!book.items){console.log("no items :( "); return;}
 
-    fs.appendFile('./isbnoutput.txt', response, err => {
-            if (err) {
-              console.error("write error ", err);
-            } else {
-              console.error("write success 💃")
-            }
-          });
-
-    // none of this works. response is not parsable in this way.
-    if(!response.items){console.log("no items :( "); return;}
-
-    for (var i = 0; i < response.items.length; i++) {
-        var item = response.items[i];
+    for (var i = 0; i < book.items.length; i++) {
+        var item = book.items[i];
         for (var j = 0; j < item.volumeInfo.industryIdentifiers.length; j++){
           if(item.volumeInfo.industryIdentifiers[j].identifier == "9781581574036"){
             console.log("got it")
@@ -41,16 +32,16 @@ const req = https.request(options, (res) => {
 
           fs.appendFile('/Users/glaistig/fun-codez/isbnoutput.txt', content, err => {
             if (err) {
-              console.error(err);
+              console.error('Write error: ', err);
             } else {
-              // file written successfully
+              console.log('Write success 🕺')
             }
           });
           console.log("i ", i, " id ", item.volumeInfo.industryIdentifiers[j].identifier)
         }
 
       }
-  });
+    });
 });
 
 req.on('error', (e) => {
