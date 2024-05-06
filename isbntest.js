e6c2e47944b05378e179fa07cef022cda83ceebf
cheri marie <cherimarie@gmail.com>
2024-05-05 22:39:49 -0700

    Working as expected; output file is not yet usable

--- isbntest.js
+++ isbntest.js
@@ -7,44 +7,52 @@ const options = {
   path: '/books/v1/volumes?q=+isbn:9781581574036',
   method: 'GET',
 };
-let book;
+let book = '';
 
 const req = https.request(options, (res) => {
   console.log('statusCode:', res.statusCode);
+  res.setEncoding('utf8');
+  let rawData = '';
 
-  res.on('data', (response) => {
-    book += response;
+  res.on('data', (chunk) => {
+    rawData += chunk;
   });
 
   res.on('end', () => {
-    console.log(book);
-    // let response = JSON.parse(d); ??
-    if(!book.items){console.log("no items :( "); return;}
 
-    for (var i = 0; i < book.items.length; i++) {
+    try {
+      const book = JSON.parse(rawData);
+      // console.log(book.items);
+      if(!book.items){console.log("no items :( "); return;}
+      for (var i = 0; i < book.items.length; i++) {
         var item = book.items[i];
         for (var j = 0; j < item.volumeInfo.industryIdentifiers.length; j++){
           if(item.volumeInfo.industryIdentifiers[j].identifier == "9781581574036"){
             console.log("got it")
             // in production code, item.text should have the HTML entities escaped.
-          let content = `${item.volumeInfo.title} ${item.volumeInfo.subtitle}, ${item.volumeInfo.authors[0]}, ${item.volumeInfo.description}`;
-          }
+            let content = `${item.volumeInfo.title} ${item.volumeInfo.subtitle} \n ${item.volumeInfo.authors[0]} \n ${item.volumeInfo.description}`;
 
-          fs.appendFile('/Users/glaistig/fun-codez/isbnoutput.txt', content, err => {
-            if (err) {
-              console.error('Write error: ', err);
-            } else {
-              console.log('Write success 🕺')
-            }
-          });
+            fs.appendFile('./isbnoutput.txt', content, err => {
+              if (err) {
+                console.error('Write error: ', err);
+              } else {
+                console.log('Write success 🕺')
+              }
+            });
+          }
           console.log("i ", i, " id ", item.volumeInfo.industryIdentifiers[j].identifier)
         }
 
       }
-    });
+    } catch (e) {
+      console.error(e.message);
+    }
+
+  });
 });
 
 req.on('error', (e) => {
   console.error(e);
 });
 req.end();
+
