6236110c87bc6dc357d4021e71acac7457a9507b
cheri marie <cherimarie@gmail.com>
2024-05-05 22:54:13 -0700

    Works with an array of isbns

--- isbntest.js
+++ isbntest.js
@@ -2,57 +2,64 @@ const fs = require('node:fs');
 
 const https = require('node:https');
 
-const options = {
-  hostname: 'www.googleapis.com',
-  path: '/books/v1/volumes?q=+isbn:9781581574036',
-  method: 'GET',
-};
-let book = '';
+let isbns = ["9781581574036"]
 
-const req = https.request(options, (res) => {
-  console.log('statusCode:', res.statusCode);
-  res.setEncoding('utf8');
-  let rawData = '';
+function getBook(isbn){
+  const options = {
+    hostname: 'www.googleapis.com',
+    path: `/books/v1/volumes?q=+isbn:${isbn}`,
+    method: 'GET',
+  };
 
-  res.on('data', (chunk) => {
-    rawData += chunk;
-  });
+  const req = https.request(options, (res) => {
+    console.log('statusCode:', res.statusCode);
+    res.setEncoding('utf8');
+    let rawData = '';
 
-  res.on('end', () => {
+    res.on('data', (chunk) => {
+      rawData += chunk;
+    });
 
-    try {
-      const book = JSON.parse(rawData);
-      // console.log(book.items);
-      if(!book.items){console.log("no items :( "); return;}
-      for (var i = 0; i < book.items.length; i++) {
-        var item = book.items[i];
-        for (var j = 0; j < item.volumeInfo.industryIdentifiers.length; j++){
-          if(item.volumeInfo.industryIdentifiers[j].identifier == "9781581574036"){
-            console.log("got it")
-            // in production code, item.text should have the HTML entities escaped.
-            let content = `${item.volumeInfo.title} ${item.volumeInfo.subtitle} *${item.volumeInfo.authors[0]} *${item.volumeInfo.description}`;
+    res.on('end', () => {
 
-            fs.appendFile('./output.csv', content, err => {
-              if (err) {
-                console.error('Write error: ', err);
-              } else {
-                console.log('Write success 🕺')
-              }
-            });
+      try {
+        const book = JSON.parse(rawData);
+
+        if(!book.items){console.log("no items :( "); return;}
+
+        for (var i = 0; i < book.items.length; i++) {
+          var item = book.items[i];
+          for (var j = 0; j < item.volumeInfo.industryIdentifiers.length; j++){
+            console.log("i ", i, " id ", item.volumeInfo.industryIdentifiers[j].identifier)
+
+            if(item.volumeInfo.industryIdentifiers[j].identifier == isbn){
+              console.log("got it")
+
+              let content = `\n${item.volumeInfo.title} ${item.volumeInfo.subtitle}*${item.volumeInfo.authors[0]}*${item.volumeInfo.description}*${isbn}`;
+
+              fs.appendFile('./output.csv', content, err => {
+                if (err) {
+                  console.error('Write error: ', err);
+                } else {
+                  console.log('Write success 🕺')
+                }
+              });
+            }
+
           }
-          console.log("i ", i, " id ", item.volumeInfo.industryIdentifiers[j].identifier)
+
         }
-
+      } catch (e) {
+        console.error(e.message);
       }
-    } catch (e) {
-      console.error(e.message);
-    }
 
+    });
   });
-});
 
-req.on('error', (e) => {
-  console.error(e);
-});
-req.end();
+  req.on('error', (e) => {
+    console.error(e);
+  });
+  req.end();
+}
 
+isbns.forEach((i) => getBook(i))
